19b4ff35b1f2d4f9db3ff7363a77f250367fd3b3
Saranya2627 <245123751008@mvsrec.edu.in>
2026-03-09 15:32:22 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,7 @@
+public class Details {
+    public static void main(String[] args) {
+        System.out.println("Name: Saranya");
+        System.out.println("College: ABC College");
+        System.out.println("Branch: CSE");
+    }
+}
